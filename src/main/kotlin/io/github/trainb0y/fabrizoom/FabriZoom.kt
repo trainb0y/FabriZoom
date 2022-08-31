abda3be63b11c0b0e1698224bcb050c1e131674c
trainb0y <trainb0ydev@gmail.com>
2022-08-30 19:38:47 -0600

    Remove ZoomKeybinds

--- src/main/kotlin/io/github/trainb0y/fabrizoom/FabriZoom.kt
+++ src/main/kotlin/io/github/trainb0y/fabrizoom/FabriZoom.kt
@@ -1,9 +1,12 @@
 package io.github.trainb0y.fabrizoom
 
-import io.github.trainb0y.fabrizoom.keybinds.ZoomKeybinds
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.util.InputUtil
+import org.lwjgl.glfw.GLFW
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -11,13 +14,16 @@ import org.slf4j.LoggerFactory
 class FabriZoom : ClientModInitializer {
 	override fun onInitializeClient() {
 		ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client: MinecraftClient? ->
-			val shouldZoom = ZoomKeybinds.zoomKey.isPressed
+			val shouldZoom = zoomKey.isPressed
 			Zoom.zooming = shouldZoom
 			if (!shouldZoom) Zoom.currentZoomFovMultiplier = 1f
 		})
 	}
 
 	companion object {
+		val zoomKey: KeyBinding = KeyBindingHelper.registerKeyBinding(
+			KeyBinding("key.fabrizoom.zoom", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "key.fabrizoom.category")
+		)
 		val logger: Logger = LoggerFactory.getLogger("fabrizoom")
 	}
 }
