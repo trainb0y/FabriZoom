66ab7e289aebd7ed6f0dfeb35d7c466bc1de0d3a
trainb0y <trainb0ydev@gmail.com>
2022-09-10 16:16:28 -0600

    Reset zoom divisor when zoom ends

--- src/main/kotlin/io/github/trainb0y/fabrizoom/FabriZoom.kt
+++ src/main/kotlin/io/github/trainb0y/fabrizoom/FabriZoom.kt
@@ -1,5 +1,6 @@
 package io.github.trainb0y.fabrizoom
 
+import io.github.trainb0y.fabrizoom.config.Config
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
@@ -14,6 +15,9 @@ import org.slf4j.LoggerFactory
 class FabriZoom : ClientModInitializer {
 	override fun onInitializeClient() {
 		ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client: MinecraftClient? ->
+			if (!zoomKey.isPressed && ZoomLogic.zooming) {
+				ZoomLogic.zoomDivisor = Config.zoomDivisor
+			}
 			ZoomLogic.zooming = zoomKey.isPressed
 		})
 	}
