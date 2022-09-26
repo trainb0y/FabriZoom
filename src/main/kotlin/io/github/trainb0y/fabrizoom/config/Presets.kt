4ef9757ca537310aeb0e6640b0839bdaa8430fd3
trainb0y <trainb0ydev@gmail.com>
2022-09-26 10:49:56 -0600

    Cinematic and Quick Toggle presets

--- src/main/kotlin/io/github/trainb0y/fabrizoom/config/Presets.kt
+++ src/main/kotlin/io/github/trainb0y/fabrizoom/config/Presets.kt
@@ -16,11 +16,28 @@ enum class Presets(val key: String, val values: ConfigurableValues?) {
 			0.4f,
 			1.0,
 			false,
+			Config.Transition.LINEAR
+		)
+	),
+	CINEMATIC(
+		"presets.fabrizoom.cinematic",
+		ConfigurableValues(
+			30,
+			true,
+			4.0,
+			4.0,
+			3.0,
+			20.0,
+			0.125,
+			0.25,
+			0.4f,
+			1.0,
+			false,
 			Config.Transition.SMOOTH
 		)
 	),
-	OPTIFINE(
-		"presets.fabrizoom.optifine",
+	QUICK_TOGGLE(
+		"presets.fabrizoom.quicktoggle",
 		ConfigurableValues(
 			30,
 			false,
@@ -33,7 +50,7 @@ enum class Presets(val key: String, val values: ConfigurableValues?) {
 			0.4f,
 			1.0,
 			false,
-			Config.Transition.SMOOTH
+			Config.Transition.NONE
 		)
 	),
 	CUSTOM(
