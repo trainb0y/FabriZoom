3aebabcfde3daf88754086220db4003d66dfe00c
trainb0y <trainb0ydev@gmail.com>
2023-10-29 17:38:23 -0600

    fix client command on Forge

--- forge/src/main/kotlin/io/github/trainb0y/fabrizoom/forge/ForgePlatform.kt
+++ forge/src/main/kotlin/io/github/trainb0y/fabrizoom/forge/ForgePlatform.kt
@@ -7,6 +7,7 @@ import io.github.trainb0y.fabrizoom.Platform
 import net.minecraft.client.Minecraft
 import net.minecraft.commands.CommandSourceStack
 import net.minecraftforge.client.ClientCommandHandler
+import net.minecraftforge.client.event.RegisterClientCommandsEvent
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.TickEvent
@@ -26,6 +27,11 @@ class ForgePlatform : Platform {
 			fun onClientTick(event: TickEvent.ClientTickEvent) {
 				if (event.phase == TickEvent.Phase.END && ::onTick.isInitialized) onTick(Minecraft.getInstance())
 			}
+			@SubscribeEvent
+			fun onAddClientCommands(event: RegisterClientCommandsEvent) = event.dispatcher.register(literal<CommandSourceStack>("fabrizoom").executes {
+				FabriZoom.shouldOpenConfigScreen = true
+				1
+			})
 		})
 		MOD_CONTEXT.getKEventBus().register(object {
 			@SubscribeEvent
@@ -35,13 +41,8 @@ class ForgePlatform : Platform {
 
 	override fun getConfigPath(): Path = FMLPaths.CONFIGDIR.get().resolve("fabrizoom.json")
 	override fun registerKeybinds() {} // can be ignored, on forge we use an event to do it instead
+	override fun registerCommand() {} // also ignored, uses forge event
 	override fun registerTick(onTick: (Minecraft) -> Unit) {
 		this.onTick = onTick
-	}
-	override fun registerCommand() {
-		ClientCommandHandler.getDispatcher().register(literal<CommandSourceStack>("fabrizoom").executes {
-			FabriZoom.shouldOpenConfigScreen = true
-			1
-		})
 	}
 }
